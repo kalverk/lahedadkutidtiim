e0f85fbd261f92668e5cd70d56bd1c0ad24983ca
kalverk <kalverk@hotmail.com>
2014-02-23 19:38:46 +0200

    fb login tehtud
    pärast logimist veel suurt midagi ei tee aga küll jõuab. Sourcetree on
    nõme ja jube aeglane programm nii, et hakkasin giti pluginit kasutama,
    loodan, et see mingeid erroreid ei too :)

---
+++ kaart/war/js/facebook.js
@@ -0,0 +1,39 @@
+init = false;
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '274740106025651', //võiks mingi faili teha, kust sensitive infot võetakse?
+		status : true,
+		cookie : true,
+		xfbml : true
+	});
+	init = true;
+};
+
+function login(callBack) {
+	if (!init) {
+		window.fbAsyncInit();
+	}
+	FB.login(function(response) {
+		if (response.authResponse) {
+			FB.api('/me', function(response) {
+				alert('Good to see you, ' + response.name);
+			});
+		}
+	}, {
+		perms : ""
+	});
+
+}
+
+// Load the SDK Asynchronously
+(function(d) {
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement('script');
+	js.id = id;
+	js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
