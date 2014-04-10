99fd12b92d74baa4a5f879c45ab9c29eb7d4ebda
Markus Kängsepp <markus9696@gmail.com>
2014-04-10 19:28:55 +0300

    Ajaxlinking.

--- kaart/war/js/ajaxlinking.js
+++ kaart/war/js/ajaxlinking.js
@@ -1,19 +1,19 @@
 function handleChange(evt) {
-	
-	
-	var categories=["football", "basketball", "volleyball", "tennis", "tabletennis", "pool", 
-	                "bowling", "golf", "hockey", "baseball", "crosscountry", "iceskating", "snowboard",
-	                "skatepark", "archery", "bicycle", "camping", "diving", "rowing", "skating","swimming", "running", "gym"];
 
-	
-	if (evt.pathNames[0] == 'login-box'){
-		 showLoginForm('a.login-window');
-	}else if($.inArray(evt.pathNames[0], categories)>= 0){
+	var categories = [ "football", "basketball", "volleyball", "tennis",
+			"tabletennis", "pool", "bowling", "golf", "hockey", "baseball",
+			"crosscountry", "iceskating", "snowboard", "skatepark", "archery",
+			"bicycle", "camping", "diving", "rowing", "skating", "swimming",
+			"running", "gym" ];
+
+	if (evt.pathNames[0] == 'login-box') {
+		showLoginForm('a.login-window');
+	} else if ($.inArray(evt.pathNames[0], categories) >= 0) {
 		getInfo(evt.pathNames[0]);
-	}else {
-	
 		closeLoginForm();
-    }
+	} else {
+		closeLoginForm();
+	}
 }
 
 SWFAddress.addEventListener(SWFAddressEvent.CHANGE, handleChange);
